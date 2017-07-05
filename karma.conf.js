cfa7df8bea6be973bff20f818ef79f3c4f913048
Jack Reed <phillipjreed@gmail.com>
2017-07-05 10:52:19 -0400

    add es5/es6 shim for phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'es6-shim'],
 
 
     // list of files / patterns to load in the browser
